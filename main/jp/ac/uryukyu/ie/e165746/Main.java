96b0ba457442717a59037c87dc1b89ccfad471a0
Tomotaka Yonaha <e165746@ie.u-ryukyu.ac.jp>
2016-12-05 16:26:53 +0900

    report5 step1 Main version 0

---
+++ main/jp/ac/uryukyu/ie/e165746/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165746;
+
+/**
+ * Created by e165746 on 2016/12/05.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.printf("%s\n",str.length());
+    }
+}

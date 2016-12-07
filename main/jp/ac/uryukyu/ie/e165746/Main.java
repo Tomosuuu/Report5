eff816604aa14e25bc5a5f37263d457c89b57d52
Tomotaka Yonaha <e165746@ie.u-ryukyu.ac.jp>
2016-12-07 18:19:39 +0900

    report5 step2 Main version 1

--- main/jp/ac/uryukyu/ie/e165746/Main.java
+++ main/jp/ac/uryukyu/ie/e165746/Main.java
@@ -3,9 +3,16 @@ package jp.ac.uryukyu.ie.e165746;
 /**
  * Created by e165746 on 2016/12/05.
  */
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.printf("%s\n",str.length());
+        try{
+            String str = null;
+            System.out.printf("%s\n", str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生した。\n" );
+            e.printStackTrace();
+        }
     }
 }
